04aa0a9334acc2571d01da2a61560146d5ba070d
KrutikaShah52 <krutika.shah@einfochips.com>
2018-02-08 14:13:15 +0530

    change is rollback

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   	 resources :demo_scaffolds
   		  # root :controller => 'demo_scaffolds'
-  		  # root 'demo_scaffolds#index'
+  		   root 'demo_scaffolds#index'
   		  # get 'demo_scaffolds'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
